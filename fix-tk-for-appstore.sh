5d66866d650c05ab63b0fa26d2d315ab02103172
Adam Kessel <adam@rosi-kessel.org>
2026-05-03 21:12:59 -0400

    minor fixes

--- fix-tk-for-appstore.sh
+++ fix-tk-for-appstore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Usage: ${0} [-c] [path to Python frameworks]"
 printf "\nThis script fixes Python tcl/tk so that an application built with pyinstaller can be submitted to the App Store."
 echo "This is necessary because tcl8.6 includes a reference to NSWindowDidOrderOnScreenNotification which is a deprecated API. It does not appear to be used by Tk so can be commented out."
@@ -15,19 +15,19 @@ echo "Downloading tcl and tk."
 [ -e tcl8.6.17-src.tar.gz ] || curl -L -sS -o tcl8.6.17-src.tar.gz 'https://downloads.sourceforge.net/project/tcl/Tcl/8.6.17/tcl8.6.17-src.tar.gz'
 [ -e tk8.6.17-src.tar.gz ] || curl -L -sS -o tk8.6.17-src.tar.gz 'https://downloads.sourceforge.net/project/tcl/Tcl/8.6.17/tk8.6.17-src.tar.gz'
 tar xfz tcl8.6.17-src.tar.gz || {
-  echo 'Extracting of tcl source code failed. Exiting.'
-  exit 1
+	echo 'Extracting of tcl source code failed. Exiting.'
+	exit 1
 }
 tar xfz tk8.6.17-src.tar.gz || {
-  echo 'Extracting of tk source code failed. Exiting.'
-  exit 1
+	echo 'Extracting of tk source code failed. Exiting.'
+	exit 1
 }
 [ -e tcl8.6.17/macosx/GNUmakefile ] && [ -e tk8.6.17/macosx/GNUmakefile ] || {
-  echo 'GNUmakefile not found. Something went wrong. Exiting.'
-  exit 1
+	echo 'GNUmakefile not found. Something went wrong. Exiting.'
+	exit 1
 }
 echo "Patching source code."
-patch -t << 'EOF'
+patch -t <<'EOF'
 diff -r -u tcl8.6.17/generic/tcl.h tcl8.6.17-appstore/generic/tcl.h
 --- tcl8.6.17/generic/tcl.h	2025-07-31 13:29:02
 +++ tcl8.6.17-appstore/generic/tcl.h	2026-05-03 13:19:00
@@ -79,46 +79,46 @@ EOF
 export CFLAGS="-arch x86_64 -arch arm64"
 echo "Building tcl. This may take a while."
 cd tcl8.6.17/macosx
-make deploy install INSTALL_ROOT="${output_folder}" >> ../../tcl-build.log 2>&1 || {
-  echo "Error occurred building tcl. Check tcl-build.log."
-  exit 1
+make deploy install INSTALL_ROOT="${output_folder}" >>../../tcl-build.log 2>&1 || {
+	echo "Error occurred building tcl. Check tcl-build.log."
+	exit 1
 }
 cd ../../tk8.6.17/macosx
 echo "Building tk. This may take a while."
-make deploy install INSTALL_TARGETS='install-binaries install-libraries install-headers install-private-headers' INSTALL_ROOT="${output_folder}" >> ../../tk-build.log 2>&1 || {
-  echo "Error occurred building tk. Check tk-build.log."
-  exit 1
+make deploy install INSTALL_TARGETS='install-binaries install-libraries install-headers install-private-headers' INSTALL_ROOT="${output_folder}" >>../../tk-build.log 2>&1 || {
+	echo "Error occurred building tk. Check tk-build.log."
+	exit 1
 }
 cd ../..
 echo "tcl and tk built successfully. Installing in Python frameworks directory."
 
 backup_path="${framework_path}/backups/$(date +'%Y-%m-%d-%H-%M-%S')"
-tcl_path=$(realpath "${framework_path}/Tcl.framework")
-tk_path=$(realpath "${framework_path}/Tk.framework")
+tcl_path="$(realpath ${framework_path}/Tcl.framework)"
+tk_path="$(realpath ${framework_path}/Tk.framework)"
 [ -n "${tcl_path}" ] && [ -n "${tk_path}" ] || {
-  echo "Could not find existing Tcl/Tk paths under ${framework_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
-  exit 1
+	echo "Could not find existing Tcl/Tk paths under ${framework_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
+	exit 1
 }
 [ -d "${tk_path}" ] || {
-  echo "Could not find ${tk_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
-  exit 1
+	echo "Could not find ${tk_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
+	exit 1
 }
 echo "Found tk in ${tk_path}."
 [ -d "${tcl_path}" ] || {
-  echo "Could not find ${tcl_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
-  exit 1
+	echo "Could not find ${tcl_path}. If Python is installed elsewhere, please re-run this script with the the path to its 'Frameworks' folder."
+	exit 1
 }
 [ -e "${output_folder}/Library/Frameworks/Tk.framework/Versions/8.6/Tk" ] && [ -e "${output_folder}/Library/Frameworks/Tcl.framework/Versions/8.6/Tcl" ] || {
-  echo "Tk/Tcl output not built as expected in ${output_folder}/Library/Frameworks/. Check build logs and try again."
-  exit 1
+	echo "Tk/Tcl output not built as expected in ${output_folder}/Library/Frameworks/. Check build logs and try again."
+	exit 1
 }
 echo "Found tcl in ${tcl_path}."
 echo "Installing patched Tk and Tcl into ${framework_path}. This requires sudo privileges, so you may be prompted for your password."
 echo "Backing up current Tk and Tcl Python frameworks to ${backup_path}. These can be deleted manually if you like."
 sudo mkdir -p "${backup_path}"
 sudo cp -R "${tk_path}" "${tcl_path}" "${backup_path}" || {
-  echo "Backup failed. Exiting."
-  exit 1
+	echo "Backup failed. Exiting."
+	exit 1
 }
 echo "Backup successful. Updating dylib paths."
 sudo install_name_tool -id "${tk_path}/Tk" "${output_folder}/Library/Frameworks/Tk.framework/Versions/8.6/Tk"
@@ -126,8 +126,8 @@ sudo install_name_tool -id "${tcl_path}/Tcl" "${output_folder}/Library/Framework
 echo "Replacing Tk and Tcl with patched versions."
 sudo rm -rf "${tcl_path}" "${tk_path}"
 sudo cp -R "${output_folder}/Library/Frameworks/Tk.framework" "${output_folder}/Library/Frameworks/Tcl.framework" "${framework_path}" || {
-  echo "Copy failed. Exiting."
-  exit 1
+	echo "Copy failed. Exiting."
+	exit 1
 }
 echo "Signing patched Tk and Tcl."
 sudo codesign --force --deep --sign - "${tcl_path}" || echo "Tcl signing failed."
